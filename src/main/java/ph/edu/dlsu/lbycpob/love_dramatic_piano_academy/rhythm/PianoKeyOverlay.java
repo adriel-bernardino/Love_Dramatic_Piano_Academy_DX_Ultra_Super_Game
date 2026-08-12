9e516e35560f66609448475e7cb68b310525ecbe
Mark Cloma Jr. <mark_clomajr@dlsu.edu.ph>
2026-08-13 03:52:30 +0800

    created expected class files

---
+++ src/main/java/ph/edu/dlsu/lbycpob/love_dramatic_piano_academy/rhythm/PianoKeyOverlay.java
@@ -0,0 +1,4 @@
+package ph.edu.dlsu.lbycpob.love_dramatic_piano_academy.rhythm;
+
+public class PianoKeyOverlay {
+}
---
+++ src/main/java/ph/edu/dlsu/lbycpob/love_dramatic_piano_academy/vn/ChoicePrompt.java
@@ -0,0 +1,4 @@
+package ph.edu.dlsu.lbycpob.love_dramatic_piano_academy.vn;
+
+public class ChoicePrompt {
+}
