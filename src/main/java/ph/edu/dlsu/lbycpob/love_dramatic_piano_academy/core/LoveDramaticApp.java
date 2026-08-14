671eedf14cf602a2fb14e0665143dc5dd2307c4a
Mark Cloma Jr. <mark_clomajr@dlsu.edu.ph>
2026-08-14 16:29:13 +0800

    edited to accomodate new scene controller

--- src/main/java/ph/edu/dlsu/lbycpob/love_dramatic_piano_academy/core/LoveDramaticApp.java
+++ src/main/java/ph/edu/dlsu/lbycpob/love_dramatic_piano_academy/core/LoveDramaticApp.java
@@ -3,46 +3,37 @@ package ph.edu.dlsu.lbycpob.love_dramatic_piano_academy.core;
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.dsl.FXGL;
-import javafx.scene.text.Text;
-import ph.edu.dlsu.lbycpob.love_dramatic_piano_academy.rhythm.RhythmState;
-import ph.edu.dlsu.lbycpob.love_dramatic_piano_academy.vn.VNState;
 
 public class LoveDramaticApp extends GameApplication {
 
-    // Understand: Sets up all
+    // Understand: Keep the enum here so it can be referenced globally if needed
     public enum GameState { MAIN_MENU, VISUAL_NOVEL, RHYTHM_GAME }
-    private GameState currentState = GameState.MAIN_MENU;
 
     // Understand: various variables that will be needed throughout the game
     private int storyChapter = 0;
     private char route = 'A';
     private boolean isPaused = false;
 
-    private MainMenuState mainMenu;
-    private VNState vnState;
-    private RhythmState rhythmState;
+    // Understand: The centralized scene manager replaces individual state variables
+    private CoreSceneManager sceneManager;
 
     @Override
     protected void initSettings(GameSettings settings) {
         settings.setWidth(1920);
         settings.setHeight(1080);
         settings.setTitle("Love Dramatic Piano Academy");
-
     }
 
     @Override
     protected void initGame() {
-
         // Understand: Register entity factories centrally
         FXGL.getGameWorld().addEntityFactory(new BackgroundManager());
 
-        mainMenu = new MainMenuState();
-        vnState = new VNState();
-        rhythmState = new RhythmState();
+        // Understand: Initialize the manager, which in turn initializes the states
+        sceneManager = new CoreSceneManager();
 
-        // Understand: Start the Main Menu state on initial launch
-        mainMenu.start();
-
+        // Understand: Boot the game into the main menu
+        sceneManager.startInitialState();
     }
 
     public static void main(String[] args) {
