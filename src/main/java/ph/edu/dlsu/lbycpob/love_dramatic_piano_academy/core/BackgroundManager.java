8ffdb66b4290765ba5ac0cd471260d73745698e9
Mark Cloma Jr. <mark_clomajr@dlsu.edu.ph>
2026-08-14 16:03:08 +0800

    made BackgroundManager implement EntityFactory to create different background based on context

--- src/main/java/ph/edu/dlsu/lbycpob/love_dramatic_piano_academy/core/BackgroundManager.java
+++ src/main/java/ph/edu/dlsu/lbycpob/love_dramatic_piano_academy/core/BackgroundManager.java
@@ -1,4 +1,29 @@
 package ph.edu.dlsu.lbycpob.love_dramatic_piano_academy.core;
 
-public class BackgroundManager {
+import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.EntityFactory;
+import com.almasb.fxgl.entity.SpawnData;
+import com.almasb.fxgl.entity.Spawns;
+import com.almasb.fxgl.texture.Texture;
+
+// Understand: creates different background entities depending on the context
+public class BackgroundManager implements EntityFactory {
+
+    @Spawns("background")
+    public Entity newEntity(SpawnData data) {
+        // Understand: Reads "imageName" from SpawnData if present, otherwise default to "VNbgs/placeholder.png"
+        String imageName = data.hasKey("imageName")
+                ? data.get("imageName")
+                : "VNbgs/placeholder.png";
+
+        // Understand: Sets background to the custom one defined in imageName
+        Texture bg = FXGL.texture(imageName);
+
+        // Understand: creates the entity, puts it at the very back using z index
+        return FXGL.entityBuilder(data)
+                .view(bg)
+                .zIndex(-100)
+                .build();
+    }
 }
