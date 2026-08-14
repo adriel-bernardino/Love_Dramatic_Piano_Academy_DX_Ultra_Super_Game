e3a8cdef78e370c12790c571cf35f9607b3b9aee
Mark Cloma Jr. <mark_clomajr@dlsu.edu.ph>
2026-08-14 14:53:07 +0800

    deleted for now because theye are causing errors

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module ph.edu.dlsu.lbycpob.love_dramatic_piano_academy {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.almasb.fxgl.all;
-
-    opens ph.edu.dlsu.lbycpob.love_dramatic_piano_academy to javafx.fxml;
-    exports ph.edu.dlsu.lbycpob.love_dramatic_piano_academy;
-}
