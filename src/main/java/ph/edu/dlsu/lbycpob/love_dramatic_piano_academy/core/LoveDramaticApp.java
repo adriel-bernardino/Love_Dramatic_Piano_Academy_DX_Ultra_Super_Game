f9dd61cf58b31691e2bd418c930618ede575f847
Mark Cloma Jr. <mark_clomajr@dlsu.edu.ph>
2026-08-14 15:32:58 +0800

    initialized the various variables that will be used, gamesettings, and gamestates

--- src/main/java/ph/edu/dlsu/lbycpob/love_dramatic_piano_academy/core/LoveDramaticApp.java
+++ src/main/java/ph/edu/dlsu/lbycpob/love_dramatic_piano_academy/core/LoveDramaticApp.java
@@ -1,4 +1,42 @@
 package ph.edu.dlsu.lbycpob.love_dramatic_piano_academy.core;
 
-public class LoveDramaticApp {
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+import javafx.scene.text.Text;
+import ph.edu.dlsu.lbycpob.love_dramatic_piano_academy.rhythm.RhythmState;
+import ph.edu.dlsu.lbycpob.love_dramatic_piano_academy.vn.VNState;
+
+public class LoveDramaticApp extends GameApplication {
+
+    // Understand: Sets up all
+    public enum GameState { MAIN_MENU, VISUAL_NOVEL, RHYTHM_GAME }
+    private GameState currentState = GameState.MAIN_MENU;
+
+    // Understand: various variables that will be needed throughout the game
+    private int storyChapter = 0;
+    private char route = 'A';
+    private boolean isPaused = false;
+
+    private MainMenuState mainMenu;
+    private VNState vnState;
+    private RhythmState rhythmState;
+
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setWidth(1920);
+        settings.setHeight(1080);
+        settings.setTitle("Love Dramatic Piano Academy");
+
+    }
+
+    @Override
+    protected void initGame() {
+        mainMenu = new MainMenuState();
+        vnState = new VNState();
+        rhythmState = new RhythmState();
+
+
+
+
+    }
 }
