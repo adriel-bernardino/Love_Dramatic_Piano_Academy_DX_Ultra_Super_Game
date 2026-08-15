7560819c1d97c44ab1d5fa2455d0052eeda902bd
Mark Cloma Jr. <mark_clomajr@dlsu.edu.ph>
2026-08-15 20:47:02 +0800

    created new game state save menu to spread tasks out for easier debugging

---
+++ src/main/java/ph/edu/dlsu/lbycpob/love_dramatic_piano_academy/core/SaveMenuState.java
@@ -0,0 +1,57 @@
+package ph.edu.dlsu.lbycpob.love_dramatic_piano_academy.core;
+
+import com.almasb.fxgl.dsl.FXGL;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import ph.edu.dlsu.lbycpob.love_dramatic_piano_academy.shared.SaveData;
+
+// Understand: Dedicated state for saving to multiple slots using checkpoint data
+public class SaveMenuState {
+
+    private final CoreSceneManager sceneManager;
+    private final SaveManager saveManager;
+    private VBox uiBox;
+
+    private int pendingChapterId;
+    private char pendingRoute;
+    private int pendingLine;
+
+    public SaveMenuState(CoreSceneManager sceneManager) {
+        this.sceneManager = sceneManager;
+        this.saveManager = new SaveManager();
+    }
+
+    // Understand: Receives the checkpoint data from the current chapter
+    public void start(int chapterId, char route, int checkpointLine) {
+        this.pendingChapterId = chapterId;
+        this.pendingRoute = route;
+        this.pendingLine = checkpointLine;
+
+        uiBox = new VBox(15);
+        uiBox.setTranslateX(FXGL.getAppWidth() / 2.0 - 100);
+        uiBox.setTranslateY(FXGL.getAppHeight() / 2.0 - 100);
+
+        for (int i = 1; i <= 3; i++) {
+            final int slot = i;
+            Button slotBtn = FXGL.getUIFactoryService().newButton("Save to Slot " + slot);
+            slotBtn.setOnAction(e -> {
+                SaveData data = new SaveData(pendingChapterId, String.valueOf(pendingRoute), String.valueOf(pendingLine), slot);
+                saveManager.saveGame(data);
+
+                // Understand: Return immediately to the VN after saving
+                sceneManager.resumeVisualNovel();
+            });
+            uiBox.getChildren().add(slotBtn);
+        }
+
+        Button cancelBtn = FXGL.getUIFactoryService().newButton("Cancel");
+        cancelBtn.setOnAction(e -> sceneManager.resumeVisualNovel());
+        uiBox.getChildren().add(cancelBtn);
+
+        FXGL.addUINode(uiBox);
+    }
+
+    public void cleanup() {
+        if (uiBox != null) FXGL.removeUINode(uiBox);
+    }
+}
