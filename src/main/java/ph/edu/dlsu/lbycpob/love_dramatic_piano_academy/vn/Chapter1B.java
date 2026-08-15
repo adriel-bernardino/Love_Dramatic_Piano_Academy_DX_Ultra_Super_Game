1cfe2bc883735819a734ff35827326b8d9d3bf3b
Amanda Tolentino <amanda_tolentino@dlsu.edu.ph>
2026-08-16 05:25:02 +0800

    added util fields

--- src/main/java/ph/edu/dlsu/lbycpob/love_dramatic_piano_academy/vn/Chapter1B.java
+++ src/main/java/ph/edu/dlsu/lbycpob/love_dramatic_piano_academy/vn/Chapter1B.java
@@ -8,6 +8,12 @@ import java.util.List;
 
 public class Chapter1B extends AbstractChapter{
 
+    private final DialogueManager dialogueManager = new DialogueManager();//understand: manages & displays the dialogue
+    private CharacterSprite sprite1Overlay;//understand: stores 1st char sprite shown on screen
+    private CharacterSprite sprite2Overlay;//understand: stores 2nd char sprite shown on screen
+    private boolean isTransitioning = false;//understand: stop player from skipping while the scene is changing
+    private boolean hasReachedEnd = false; //understand: so does not go past end
+
     //understand: which story file to load
     public Chapter1B(CoreSceneManager sceneManager) {
         super(sceneManager);
