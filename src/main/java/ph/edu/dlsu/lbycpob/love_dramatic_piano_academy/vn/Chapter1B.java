70bc2202130f613b8953db274ed4f9105dbbdf91
Amanda Tolentino <amanda_tolentino@dlsu.edu.ph>
2026-08-16 05:10:01 +0800

    created class and its constructor for route b of chapter 1

---
+++ src/main/java/ph/edu/dlsu/lbycpob/love_dramatic_piano_academy/vn/Chapter1B.java
@@ -0,0 +1,37 @@
+package ph.edu.dlsu.lbycpob.love_dramatic_piano_academy.vn;
+
+import ph.edu.dlsu.lbycpob.love_dramatic_piano_academy.core.CoreSceneManager;
+import ph.edu.dlsu.lbycpob.love_dramatic_piano_academy.core.CoreSceneManager;
+import com.almasb.fxgl.entity.SpawnData;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Chapter1B extends AbstractChapter{
+
+    //understand: which story file to load
+    public Chapter1B(CoreSceneManager sceneManager) {
+        super(sceneManager);
+        loadScript("Chapter1B");
+    }
+
+
+    @Override
+    public void start(int startingLine) {
+
+    }
+
+    @Override
+    protected void advanceScript() {
+
+    }
+
+    @Override
+    protected void processCurrentLine() {
+
+    }
+
+    @Override
+    public void cleanup() {
+
+    }
+}
