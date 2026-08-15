f1a380dcf73050360c46991af24c7efef0aa12c4
Mark Cloma Jr. <mark_clomajr@dlsu.edu.ph>
2026-08-15 19:07:51 +0800

    created a character sprite handler

--- src/main/java/ph/edu/dlsu/lbycpob/love_dramatic_piano_academy/vn/CharacterSprite.java
+++ src/main/java/ph/edu/dlsu/lbycpob/love_dramatic_piano_academy/vn/CharacterSprite.java
@@ -1,4 +1,17 @@
 package ph.edu.dlsu.lbycpob.love_dramatic_piano_academy.vn;
 
-public class CharacterSprite {
+import ph.edu.dlsu.lbycpob.love_dramatic_piano_academy.core.OverlayEntity;
+
+public class CharacterSprite extends OverlayEntity {
+
+    private final String characterName;
+
+    public CharacterSprite(String characterName, String initialImagePath, double x, double y) {
+        super(initialImagePath, x, y);
+        this.characterName = characterName;
+    }
+
+    public String getCharacterName() {
+        return characterName;
+    }
 }
