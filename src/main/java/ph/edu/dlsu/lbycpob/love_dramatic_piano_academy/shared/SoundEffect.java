10e3c2fa9c34089f72182bc8235171f7b3455ee5
Amanda Tolentino <amanda_tolentino@dlsu.edu.ph>
2026-08-16 17:39:25 +0800

    added sfx files & created enum for them

---
+++ src/main/java/ph/edu/dlsu/lbycpob/love_dramatic_piano_academy/shared/SoundEffect.java
@@ -0,0 +1,24 @@
+package ph.edu.dlsu.lbycpob.love_dramatic_piano_academy.shared;
+
+//decision: enum so all sfx is defined & can be easily changed
+public enum SoundEffect {
+
+    //understand: sound used for regular button interactions
+    BUTTON_CLICK("click.mp3"),
+
+    //understand: sound used when making an important story choice
+    CHOICE_SELECT("choice_select.mp3");
+
+    //understand: stores the actual filename of the sound file
+    private final String fileName;
+
+    SoundEffect(String fileName) {
+        this.fileName = fileName;
+    }
+
+    //understand: gives the audio manager the filename it needs to load
+    public String getFileName() {
+        return fileName;
+    }
+}
+
