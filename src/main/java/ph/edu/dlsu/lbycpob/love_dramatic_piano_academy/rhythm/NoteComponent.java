0a01dbcc0bd2f6f3b1ae7bda232c85fb3ef71768
adriel-bernardino <adriel_bernardino@dlsu.edu.ph>
2026-08-18 07:29:15 +0800

    Established the necessary fields and methods for configuring the NoteComponent

--- src/main/java/ph/edu/dlsu/lbycpob/love_dramatic_piano_academy/rhythm/NoteComponent.java
+++ src/main/java/ph/edu/dlsu/lbycpob/love_dramatic_piano_academy/rhythm/NoteComponent.java
@@ -1,9 +1,6 @@
 package ph.edu.dlsu.lbycpob.love_dramatic_piano_academy.rhythm;
 
 import com.almasb.fxgl.dsl.FXGL;
-import com.almasb.fxgl.input.UserAction;
-import javafx.scene.input.KeyCode;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class NoteComponent {
@@ -11,34 +8,25 @@ public class NoteComponent {
     private String note;
     private Rectangle VisualNote;
     int lane;
+
+    public static final double noteWidth = FXGL.getAppWidth() * 0.0104;
+    public static final double noteHeight = FXGL.getAppHeight() * 0.0370;
+    public static final double spawnY = FXGL.getAppHeight()*0.01;
     NoteComponent(double timestamp, String note, int lane){
         this.timestamp = timestamp;
         this.note = note;
         this.lane = lane;
 
-        VisualNote = new Rectangle();
-    }
-
-    public enum NoteType{
-        SINGLE,
-        CHORD,
-        ARPEGGIO,
-        OCTAVE
-    }
-
-    public NoteType detectNoteType(String note){
-         if (note.contains("Arpeggio")){
-             return NoteType.ARPEGGIO;
-        } else if (note.contains("+")) {
-             return NoteType.CHORD;
-         } else if (note.contains("Octave")) {
-             return NoteType.OCTAVE;
-         }
-         else return NoteType.SINGLE;
+        VisualNote = new Rectangle(noteWidth,noteHeight);
+        VisualNote.setY(spawnY);
     }
 
     public double getTimestamp() {
         return timestamp;
+    }
+
+    public void setY(double y){
+        VisualNote.setY(y);
     }
     public String getNote() {
         return note;
