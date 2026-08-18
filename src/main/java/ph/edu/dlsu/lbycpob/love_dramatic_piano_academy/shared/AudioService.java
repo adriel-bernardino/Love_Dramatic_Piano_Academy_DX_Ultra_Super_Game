9fd608356404b49439b5681dfbd229172af08254
Amanda Tolentino <amanda_tolentino@dlsu.edu.ph>
2026-08-18 15:24:30 +0800

    add music and sfx volume controls to audio service

--- src/main/java/ph/edu/dlsu/lbycpob/love_dramatic_piano_academy/shared/AudioService.java
+++ src/main/java/ph/edu/dlsu/lbycpob/love_dramatic_piano_academy/shared/AudioService.java
@@ -6,4 +6,12 @@ public interface AudioService {
     void setPlaybackSpeed(double speed); // Accommodates 1x, 2x, 4x
     void stopMusic();
     void playSoundEffect(SoundEffect soundEffect);//understand: allows any game state to request a sound effect
+
+    //understand: controls volume of background music, 0.0 to 1.0
+    void setMusicVolume(double volume);
+    double getMusicVolume();
+
+    //understand: controls volume of sound effects, 0.0 to 1.0
+    void setSfxVolume(double volume);
+    double getSfxVolume();
 }
